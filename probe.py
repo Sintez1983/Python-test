c76ba190e4c9e66a6d2b1b8d90bd2bbb51a40bdd
MZ-ECRB-OZ-NB02 <olldey3@yandex.ru>
2023-01-21 22:24:25 +0300

    Строка на оборот
    Signed-off-by: MZ-ECRB-OZ-NB02 <olldey3@yandex.ru>

--- probe.py
+++ probe.py
@@ -1,5 +1,6 @@
 print('привет мир')
 print(b'\xd0\xbf\xd1\x80\xd0\xb8\xd0\xb2\xd0\xb5\xd1\x82' .decode('utf8'))
 print('привет' .encode())
-
-
+print(3+(2/2))
+print('приветы' [:2] + 'приветы'[4:])
+print('kinggemrkgvjerm' [::-1])
